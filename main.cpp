40f51e8121c88d39d7e4c6f4de8a8e2a9d552bd7
ArmandoIsaacParraCruz <armandoisaac.parracruz@gmail.com>
2023-07-06 18:28:07 -0600

    open file

--- main.cpp
+++ main.cpp
@@ -1,13 +1,47 @@
 #include <iostream> 
 #include <vector> 
 #include <string>
+#include <fstream>
 #include "MerkelMain.h"
 #include "OrderBookEntry.h"
 
+std::vector<std::string> tokenise(std::string csvLine, char separator) 
+{ 
+   std::vector<std::string> tokens; 
+   signed int start, end; 
+   start = csvLine.find_first_not_of(separator, 0); 
+   do { 
+      end = csvLine.find_first_of(separator, start); 
+      if(start == csvLine.length() || start == end) { 
+         break; 
+      } 
+ 
+      if(end >= 0) { 
+         tokens.push_back(csvLine.substr(start, end - start)); 
+      } else { 
+         tokens.push_back(csvLine.substr(start, csvLine.length() - start)); 
+      } 
+ 
+      start = end + 1; 
+   }while(end != std::string::npos); 
+   return tokens; 
+} 
+ 
+
+
 int main(void)
 {
-    MerkelMain app{};
-    app.init();
+    std::string csvFilenName{"20200317.csv"};
+    std::ifstream csvFile{csvFilenName};
+    std::string line;
+    if(csvFile.is_open()) {
+        std::cout << "Opend file" << csvFilenName << std::endl;
+    } else {
+        std::cout << "Problem opening file " << csvFilenName << std::endl;
+    }
+    csvFile.close();
+    /*MerkelMain app{};
+    app.init();*/
     return 0;
 }
 
